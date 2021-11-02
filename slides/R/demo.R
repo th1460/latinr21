61f2e6acb2e69d5da3a6d6e31a540adc4baa9598
Thiago-Pires <Thiago.Pires@ibm.com>
2021-11-02 20:07:18 -0300

    update slides

--- slides/R/demo.R
+++ slides/R/demo.R
@@ -19,7 +19,7 @@ titanic %<>%
 lr_mod <- logistic_reg() %>% set_engine("glm")
 lr_fit <- lr_mod %>% fit(Survived ~ Sex + Pclass, data = titanic)
 
-yaml::write_yaml(tidypredict::parse_model(lr_fit),"slides/R/my_model.yml")
+yaml::write_yaml(tidypredict::parse_model(lr_fit),"my_model.yml")
 
 # deploy
 
@@ -27,7 +27,7 @@ yaml::write_yaml(tidypredict::parse_model(lr_fit),"slides/R/my_model.yml")
 # docker push th1460/titanic
 
 # ibmcloud login
-# ibmcloud target --cf
+# ibmcloud target -o thop100@hotmail.com -s dev
 # zip -r titanic.zip exec script.R my_model.yml
 
 # ibmcloud fn action create titanic titanic.zip --docker th1460/titanic --web true
@@ -38,6 +38,6 @@ yaml::write_yaml(tidypredict::parse_model(lr_fit),"slides/R/my_model.yml")
 
 input <- list(Sex = "male", Pclass = "3rd")
 
-"https://<APIHOST>/api/v1/web/<NAMESPACE>/default/titanic.json" %>% 
+"https://us-south.functions.appdomain.cloud/api/v1/web/thop100@hotmail.com_dev/default/titanic.json" %>% 
   httr::POST(., body = input, encode = "json") %>% httr::content() %>% 
   .[c("Sex", "Pclass", "fit")] %>% jsonlite::toJSON(pretty = TRUE, auto_unbox = TRUE)
